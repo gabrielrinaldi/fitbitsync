70a90edb065a29e9c90d2455f211c439beb91b73
Gabriel Rinaldi <gabriel_rinaldi@mac.com>
2014-06-04 20:11:46 -0700

    Initial Commit

---
+++ HealthBookImporter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthBookImporter
+//
+//  Created by Gabriel Rinaldi on 6/4/14.
+//  Copyright (c) 2014 Gabriel Rinaldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
